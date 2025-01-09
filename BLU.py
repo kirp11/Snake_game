4831a0925343196ac32ecd612bf0888d18c4cd3e
Ivan K <kosi17100@gmail.com>
2025-01-10 01:40:40 +0300

    (main:BLU): Создана точка на экране игрового поля (прообраз будущей головы змеи

--- BLU.py
+++ BLU.py
@@ -9,6 +9,7 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+    pygame.draw.rect(screen, (0, 255,130), pygame.Rect(400, 300, 10, 10))
 
     pygame.display.flip()
 
