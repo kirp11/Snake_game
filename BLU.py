d0fd59b395fa878160e21ffac914289ba6a2ea8b
Ivan K <kosi17100@gmail.com>
2025-01-18 21:57:19 +0300

    (main: BLU): выделены и добавлены классы и методы классов

--- BLU.py
+++ BLU.py
@@ -1,15 +1,63 @@
 import pygame
-
+import random
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-done = False
 
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-    pygame.draw.rect(screen, (0, 255,130), pygame.Rect(400, 300, 10, 10))
+class Game:
 
-    pygame.display.flip()
 
+    done = False
+    x= 400
+    y = 300
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]: y -= 2
+        if pressed[pygame.K_DOWN]: y += 2
+        if pressed[pygame.K_LEFT]: x -= 2
+        if pressed[pygame.K_RIGHT]: x += 2
+        pygame.draw.circle(screen, (0, 255,130), (x, y), 5)
+        pygame.display.flip()
+
+class Point:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def draw(self):
+        return pygame.draw.circle(screen, self.color, (self.x, self.y), 5)
+
+    pass
+
+class Snake(Point):
+    def __init__(self):
+        self.color = (0, 255,130)
+
+    def change_dir(self):
+        pass
+
+    def add_node(self):
+        pass
+
+    pass
+
+class Border(Point):
+    def __init__(self):
+        self.color = (130, 0,20)
+
+    pass
+
+class Food(Point):
+    def __init__(self):
+        self.color = (10, 100,30)
+
+    
+    def create(self):
+
+
+    pass
