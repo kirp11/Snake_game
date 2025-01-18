f507d39897ece7d9b552896a6b707b5b1ad887e1
Ivan K <kosi17100@gmail.com>
2025-01-18 22:13:05 +0300

    (main:BLU): добавил методы классов

--- BLU.py
+++ BLU.py
@@ -2,26 +2,40 @@ import pygame
 import random
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+
 
 class Game:
+    def __init__(self):
+
+    def screen(self):
+        screen = pygame.display.set_mode((800, 600))
+        done = False
+
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+
+    def start(self):
+        pygame.display.flip()
+        pass
+
+    def play(self):
+        pass
+
+    def restart(self):
+        pass
+
+    def exit(self):
+        pass
 
 
-    done = False
-    x= 400
-    y = 300
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]: y -= 2
         if pressed[pygame.K_DOWN]: y += 2
         if pressed[pygame.K_LEFT]: x -= 2
         if pressed[pygame.K_RIGHT]: x += 2
-        pygame.draw.circle(screen, (0, 255,130), (x, y), 5)
-        pygame.display.flip()
+
 
 class Point:
     def __init__(self, x, y, color):
@@ -44,6 +58,9 @@ class Snake(Point):
     def add_node(self):
         pass
 
+    def count_food(self):
+        pass
+
     pass
 
 class Border(Point):
@@ -56,8 +73,9 @@ class Food(Point):
     def __init__(self):
         self.color = (10, 100,30)
 
-    
+
     def create(self):
+
 
 
     pass
