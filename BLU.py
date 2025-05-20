4fa5caef373c6fdb8d223ed35bc0775d951220df
Ivan K <kosi17100@gmail.com>
2025-05-21 00:46:43 +0300

    (main:BLU): создана смена направления движения сегмента при нажатии клавиш

--- BLU.py
+++ BLU.py
@@ -13,6 +13,7 @@ class Window:
     def start(self):
         screen = pygame.display.set_mode((self.lenght, self.high))
         pygame.display.set_caption(self.frase)
+        return screen
 
 class Generator_window:
     def __init__(self, event):
@@ -21,11 +22,51 @@ class Generator_window:
     def select(self):
         pass
 
+class Snake:
+    def __init__(self, screen):
+        self.screen = screen
+        self.x = 250
+        self.y = 300
+        self.delta_x = 0
+        self.delta_y = 0
 
-window = Window(500, 600, "Snake game")
+
+    def movie(self):
+        self.screen.fill((0, 0, 0))
+        pygame.draw.circle(self.screen, (0, 128, 255), (self.x, self.y), 5)
+        self.x += self.delta_x
+        self.y += self.delta_y
+        self.handler_press()
+
+    def handler_press(self):
+        pressed = pygame.key.get_pressed()
+        if  pressed[pygame.K_LEFT]:
+            self.delta_y = 0
+            self.delta_x = -10
+        if pressed[pygame.K_RIGHT]:
+            self.delta_y = 0
+            self.delta_x = 10
+        if pressed[pygame.K_UP]:
+            self.delta_x = 0
+            self.delta_y = -10
+        if pressed[pygame.K_DOWN]:
+            self.delta_x = 0
+            self.delta_y = 10
+
+
+
+
+
 running = True
+window = Window(500, 600, "Snake game")
+snake = Snake(window.start())
 while running:
-    window.start()
+    pygame.time.delay(60)
+    snake.movie()
+
+
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
