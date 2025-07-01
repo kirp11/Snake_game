93669d9eaff9a19fb5dc8bdbb0d457d64af409b2
Ivan K <kosi17100@gmail.com>
2025-07-02 00:24:34 +0300

    (main:programm): реализация основного цикла перенесена в отдельный модуль

---
+++ programm.py
@@ -0,0 +1,21 @@
+
+from src.game import  Game
+import pygame
+
+class Programm:
+
+    @staticmethod
+    def main():
+        running = True
+        game = Game()
+        while running:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT or game.window.get_frase() == "Quit":
+                    running = False
+
+            pygame.time.delay(30)
+            game.control()
+            pygame.display.update()
+
+        pygame.quit()
